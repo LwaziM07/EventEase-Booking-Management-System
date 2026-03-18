065cff1bd22b145bfb3396b990998543c3c55bd6
LwaziM07 <ST10434465@vcconnect.edu.za>
2026-03-18 19:41:35 +0200

    added commit

--- Cldv_Poe_Submission/obj/Debug/net10.0/Cldv_Poe_Submission.AssemblyInfo.cs
+++ Cldv_Poe_Submission/obj/Debug/net10.0/Cldv_Poe_Submission.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1067073c8538856f3ae886d3043bf2d8c1c554")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59e8d8cc02c24386ab5a95c1c9e0fe99d83ddb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
