2dccb84655109aee7fd045a535bdd76dcf5490cf
LwaziM07 <ST10434465@vcconnect.edu.za>
2026-03-21 14:44:27 +0200

    Added Sql file

--- Cldv_Poe_Submission/obj/Debug/net10.0/Cldv_Poe_Submission.AssemblyInfo.cs
+++ Cldv_Poe_Submission/obj/Debug/net10.0/Cldv_Poe_Submission.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbff5cf5cc6bd511203fed1185022abecf9dacd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4ce5e2582488edac1d9c01811bcba5bd4d2141")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cldv_Poe_Submission")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
